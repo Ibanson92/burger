220bc8bf651c80ce97e2f71ec51d90232fe39b4b
Ian Banson <Jayredd11@gmail.com>
2018-08-21 18:59:51 -0400

    second

--- server.js
+++ server.js
@@ -3,3 +3,30 @@ var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 var app = express();
 var orm = require("./config/orm");
+
+
+
+var controller = require("./controllers/burgers_controller");
+
+
+var PORT = process.env.PORT || 8170;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+app.use(controller);
+
+
+
+
+
+app.listen(PORT, function(){
+    console.log("Started our server and listening");
+})
+
